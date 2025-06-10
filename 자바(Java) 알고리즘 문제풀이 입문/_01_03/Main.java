f9c2a9e8eabe390c0614603af654f811ff0aef5c
bingseok <qqq7384@naver.com>
2025-06-10 13:56:55 +0900

    25.06.10 01-03

---
+++ 자바(Java) 알고리즘 문제풀이 입문/_01_03/Main.java
@@ -0,0 +1,47 @@
+package _01_03;
+
+import java.util.*;
+
+public class Main {
+    // solution1 : split
+    public String solution1(String str) {
+        String answer = "";
+
+        String[] arr = str.split(" ");
+
+        int max = Integer.MIN_VALUE;
+
+        for (String t : arr) {
+            if (t.length() >= max) {
+                answer = t;
+                max = t.length();
+            }
+        }
+        return answer;
+    }
+
+    // solution : indexOf, substring
+    public String solution2(String str) {
+        String answer = "";
+        int max = Integer.MIN_VALUE, pos;
+        while ((pos = str.indexOf(" ")) != -1) {
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+            if (len > max) {
+                max = len;
+                answer = tmp;
+            }
+            str = str.substring(pos + 1);
+        }
+
+        if (str.length() > max) answer = str;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(T.solution2(str));
+    }
+}
