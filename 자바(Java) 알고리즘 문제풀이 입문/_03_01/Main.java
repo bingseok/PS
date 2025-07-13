7c4a1f3a686ff67b8212a44ed3f4d7fda07b51e5
bingseok <qqq7384@naver.com>
2025-07-13 22:37:12 +0900

    25.07.03 03-02

--- 자바(Java) 알고리즘 문제풀이 입문/_03_01/Main.java
+++ 자바(Java) 알고리즘 문제풀이 입문/_03_01/Main.java
@@ -31,7 +31,7 @@ public class Main {
 
         return answer;
     }
-    
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
